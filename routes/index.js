fa1a3055c3dc9ec779bf8f28d90ff543288d449f
adrimual <adrimual14@gmail.com>
2020-08-05 23:05:07 +0200

    index almost done, songs completed

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,18 @@
 var express = require('express');
 var router = express.Router();
+let Song = require('../models/song');
 
 /* GET home page. */
 
-router.get('/index', function(req, res, next) {
-  res.render('index');
+router.get('/index', function (req, res, next) {
+  Song.find()
+    .then(songs => {
+      res.render('index', { songs })
+    })
+  
+  .catch (err => {
+    console.log(err)
+  })
 });
 
 module.exports = router;
